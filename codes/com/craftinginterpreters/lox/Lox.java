a3bdb6b9d10a4a06a52a2b9238687b16fa5c90f1
SHironari <cihi24007@g.nihon-u.ac.jp>
2025-01-14 11:29:41 +0900

    modify Parser.java

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -59,11 +59,21 @@ public class Lox {
 
   static void error(int line, String message) {
     report(line, "", message);
-  } private static void report(int line, String where,
+  } 
+  
+  private static void report(int line, String where,
                             String message) {
     System.err.println(
         "[line " + line + "] Error" + where + ": " + message);
     hadError = true;
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
 
 } 
