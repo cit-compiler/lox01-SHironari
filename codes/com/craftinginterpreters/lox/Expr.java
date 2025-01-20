18ec3612a817d58377036c1bbad7d3cc36190abe
SHironari <cihi24007@g.nihon-u.ac.jp>
2025-01-20 14:50:07 +0900

    Astprinter　エラー21個　Expr エラー4個

--- codes/com/craftinginterpreters/lox/Expr.java
+++ codes/com/craftinginterpreters/lox/Expr.java
@@ -32,19 +32,36 @@ abstract class Expr {
       this.expression = expression;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+
     final Expr expression;
   }
+
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
+    }
+
     final Object value;
   }
+
   static class Unary extends Expr {
     Unary(Token operator, Expr right) {
       this.operator = operator;
       this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
     }
 
     final Token operator;
