c074286702ca7244537e2e70bcaca3038b68c461
SHironari <cihi24007@g.nihon-u.ac.jp>
2025-01-21 09:42:38 +0900

    最終版

--- codes/com/craftinginterpreters/lox/Interpreter.java
+++ codes/com/craftinginterpreters/lox/Interpreter.java
@@ -1,7 +1,15 @@
 package com.craftinginterpreters.lox;
 
-class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+import com.craftinginterpreters.lox.Expr;
+import com.craftinginterpreters.lox.Stmt;
+import com.craftinginterpreters.lox.TokenType;
 
+class Interpreter implements Expr.Visitor<Object> , 
+                             Stmt.Visitor<Void> {
+  private Environment environment = new Environment();
+    
+    /*                          
     void interpret(Expr expression) {
         try {
           Object value = evaluate(expression);
@@ -10,6 +18,17 @@ class Interpreter implements Expr.Visitor<Object> {
           Lox.runtimeError(error);
         }
       }
+    */
+
+     void interpret(List<Stmt> statements) {
+    try {
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
 
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
@@ -31,6 +50,30 @@ class Interpreter implements Expr.Visitor<Object> {
       // Unreachable.
       return null;
     }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+      return environment.get(expr.name);
+    }  
+
+
+
+
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return null;
+      }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        return null;
+      }
+
+
+
+
+
 
     private void checkNumberOperand(Token operator, Object operand) {
         if (operand instanceof Double) return;
@@ -78,6 +121,60 @@ class Interpreter implements Expr.Visitor<Object> {
     private Object evaluate(Expr expr) {
         return expr.accept(this);
       }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+      }
+
+    private void executeBlock(List<Stmt> statements, Environment environment) {
+        Environment previous = this.environment;
+        try {
+          this.environment = environment;
+    
+          for (Stmt statement : statements) {
+            execute(statement);
+          }
+        } finally {
+          this.environment = previous;
+        }
+      }
+
+      @Override
+  public Void visitBlockStmt(Stmt.Block stmt) {
+    executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+      }
+
+     @Override
+      public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
+      }
+
+      @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+      Object value = evaluate(expr.value);
+      environment.assign(expr.name, value);
+      return value;
+    }
+
+    environment.define(stmt.name.lexeme, value);
+    return null;
+  }
 
       @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
@@ -127,17 +224,6 @@ class Interpreter implements Expr.Visitor<Object> {
     // Unreachable.
     return null;
   } 
-
-    private void checkNumberOperand(Token operator, Object operand) {
-        if (operand instanceof Double) return;
-        throw new RuntimeError(operator, "Operand must be a number.");
-    }
-
-    private void checkNumberOperands(Token operator, Object left, Object right) {
-        if (left instanceof Double && right instanceof Double) return;
-    
-        throw new RuntimeError(operator, "Operands must be numbers.");
-      }
 
 
 }
